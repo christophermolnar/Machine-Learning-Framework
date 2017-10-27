6545a01bfa495b15b6ec687d74abe7d75977d755
BHartford <hartbrand99@gmail.com>
2017-10-27 16:01:07 -0400

    constructor for videogames

--- src/VideoGames.java
+++ src/VideoGames.java
@@ -1,7 +1,14 @@
 
 public class VideoGames {
-	public VideoGames()
+	float price;
+	Age age;
+	int hoursOfGameplay;
+	int yearReleased;
+	public VideoGames(float price, Age age, int hoursOfGameplay, int yearReleased)
 	{
-		
+		this.price = price;
+		this.age = age;
+		this.hoursOfGameplay = hoursOfGameplay;
+		this.yearReleased = yearReleased;
 	}
 }
