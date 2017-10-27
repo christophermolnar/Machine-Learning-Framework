9a4fb300bfc4d86c1771e35df03ef5f21006ffb3
Callum <Callum@CKirbs>
2017-10-27 15:28:10 -0400

    Updated Age ENUM

--- src/Age.java
+++ src/Age.java
@@ -1,5 +1,16 @@
 
 public enum Age {
-	NEW,
-	OLD;
+	NEW("new"),
+	OLD("old");
+
+	private String age;
+
+	Age(String age){
+		this.age = age;
+	}
+	
+	public String getAge() {
+        return age;
+    }
 }
+
