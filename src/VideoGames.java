191cae02e9ddfd7faf94ecf957d9bb94e126b7dc
BHartford <hartbrand99@gmail.com>
2017-10-27 16:06:11 -0400

    videogames update

--- src/VideoGames.java
+++ src/VideoGames.java
@@ -1,14 +1,38 @@
 
 public class VideoGames {
-	float price;
+	NUMclass price;
 	Age age;
-	int hoursOfGameplay;
-	int yearReleased;
-	public VideoGames(float price, Age age, int hoursOfGameplay, int yearReleased)
+	NUMclass hoursOfGameplay;
+	NUMclass yearReleased;
+	public VideoGames(NUMclass price, Age age, NUMclass hoursOfGameplay, NUMclass yearReleased)
 	{
 		this.price = price;
 		this.age = age;
 		this.hoursOfGameplay = hoursOfGameplay;
+		this.yearReleased = yearReleased;
+	}
+	public NUMclass getPrice() {
+		return price;
+	}
+	public void setPrice(NUMclass price) {
+		this.price = price;
+	}
+	public Age getAge() {
+		return age;
+	}
+	public void setAge(Age age) {
+		this.age = age;
+	}
+	public NUMclass getHoursOfGameplay() {
+		return hoursOfGameplay;
+	}
+	public void setHoursOfGameplay(NUMclass hoursOfGameplay) {
+		this.hoursOfGameplay = hoursOfGameplay;
+	}
+	public NUMclass getYearReleased() {
+		return yearReleased;
+	}
+	public void setYearReleased(NUMclass yearReleased) {
 		this.yearReleased = yearReleased;
 	}
 }
