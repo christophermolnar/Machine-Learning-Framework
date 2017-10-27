565b9f0525ff6de7f7563f7a11177c93c6c1bd24
BHartford <hartbrand99@gmail.com>
2017-10-27 16:09:43 -0400

    added main method

--- src/Project.java
+++ src/Project.java
@@ -3,6 +3,8 @@ public class Project {
 	
 	//House h1 = new House(new POINTclass(12,25), new NUMclass(1200), new ENUMclass(NEW), new NUMclass(500000));
 	//House h2 = new House(new POINTclass(10,50), new NUMclass(1000), new ENUMclass(OLD), new NUMclass(300000));
-	
-	//ErrorTesting
+	public static void main (String args[])
+	{
+		
+	}
 }
