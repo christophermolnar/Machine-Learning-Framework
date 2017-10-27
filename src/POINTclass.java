4fc49f88121d408c9e1ed7e9273a796ef2849379
Callum <Callum@CKirbs>
2017-10-27 14:57:22 -0400

    POINTclass implemented

--- src/POINTclass.java
+++ src/POINTclass.java
@@ -1,12 +1,58 @@
-import java.awt.Point;
+import java.util.ArrayList;
 
 public class POINTclass implements Attributes {
+	private final double x;
+	private final double y;
 	
-	public POINTclass(){
-		new Point();
+	//Create new POINT
+	public POINTclass(int x, int y){
+		this.x = x;
+		this.y = y;
 	}
 	
-	public POINTclass(int x, int y){
-		new Point(x, y);
+	//Return X position of POINT
+	public double getX() {
+		return x;
+	}
+
+	//Return Y position of POINT
+	public double getY() {
+		return y;
+	}
+	
+	//Convert POINT to String
+	public String toString(){
+		return ("(" + x + "," + y + ")");
+	}
+
+	//Returns POINT closest to Compare
+	public POINTclass findClosest(ArrayList<POINTclass> Objects, POINTclass Compare){
+		POINTclass closest = null;
+		int i = 0;
+		int closestIndex = 0;
+		
+		for(POINTclass o : Objects){
+			i++;
+			if(closest == null){
+				closest = o;
+				closestIndex = i;
+			}
+			if(distance(Objects.get(i), Compare) < distance(closest, Compare)){
+				closest = o;
+				closestIndex = i;
+			}
+		}
+		return closest;
+		//return closestIndex;
+	}
+	
+	//Returns linear position of a point; sums x and y
+	public double position(){
+		return (this.getX()+this.getY())/2;
+	}
+	
+	//Returns absolte distance between two points
+	public double distance(POINTclass o, POINTclass p){
+		return Math.abs(o.position() - p.position());
 	}
 }
