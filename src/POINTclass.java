5f980c8248b3b67f7e837a566f2957b7c0d6888e
Callum <Callum@CKirbs>
2017-10-30 12:20:28 -0400

    Modified Fcn documentation

--- src/POINTclass.java
+++ src/POINTclass.java
@@ -10,22 +10,39 @@ public class POINTclass{
 		this.y = y;
 	}
 	
-	//Return X position of POINT
+	/** getX() Return X position of POINT
+	 * 
+	 * @return x : The decimal value of position X
+	 */
 	public double getX() {
 		return x;
 	}
 
-	//Return Y position of POINT
+	/** getY() Return Y position of POINT
+	 * 
+	 * @return x : The decimal value of position Y
+	 */
 	public double getY() {
 		return y;
 	}
 	
-	//Convert POINT to String
+
+	/** toString() 		Returns the num as a String value
+	 * 
+	 * @return String	Coordinate pairs X and Y as String
+	 */
 	public String toString(){
 		return ("(" + x + "," + y + ")");
 	}
 
-	//Returns POINT closest to Compare
+	//
+	
+	/** findClosest() 	Returns POINT closest to the object being compared
+	 * 
+	 * @param Objects 	ArrayList of all current POINTS to compare
+	 * @calls distance
+	 * @return int		The index in ArrayList of closest point
+	 */
 	public int findClosest(ArrayList<POINTclass> Objects){
 		POINTclass closest = null;
 		int i = 0;
@@ -46,16 +63,30 @@ public class POINTclass{
 		return closestIndex;
 	}
 	
-	//Returns linear position of a point; sums x and y
+	/** position() 		Returns euclidean distance of point X and Y
+	 * 
+	 * @return double 	Euclidean Distance of points X and Y
+	 */
 	public double position(){
 		return (this.getX()+this.getY())/2;
 	}
 	
-	//Returns absolute distance between two points
+	/** distance()		Returns absolute distance between two points
+	 * 
+	 * @param o			First POINT to compare 
+	 * @param p			Second POINT to compare
+	 * @calls position	
+	 * @return double	Absolute distance between POINT 'o' AND POINT 'p'
+	 */
 	public double distance(POINTclass o, POINTclass p){
 		return Math.abs(o.position() - p.position());
 	}
 	
+	/** equals()		(OVERRIDE) Determines if two POINTS are equal
+	 * 
+	 * @param p			POINT to check
+	 * @return			TRUE if POINTS are equal, FALSE otherwise
+	 */
 	public Boolean equals(POINTclass p){
 		if(this.getX() == p.getX() && this.getY() == p.getY())
 			return true;
