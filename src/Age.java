f1b7411331416fd20ca1b1d3827c1294a2f8ff91
Callum <Callum@CKirbs>
2017-10-30 15:16:49 -0400

    Updated Documentation

--- src/Age.java
+++ src/Age.java
@@ -1,16 +1,30 @@
 import java.util.ArrayList;
 
+/** Age(ENUM) 		Responsible for managing instances of Age String Attributes
+ * 
+ * @author MZGA
+ * @version 1.0
+ *
+ */
 public enum Age {
 	NEW("new"),
 	OLD("old");
 
 	private String age;
 
+	/** Age()		Sets the value of Age passed by user
+	 * 
+	 * @param age	Age passed by user ('new' OR 'old')
+	 */
 	Age(String age){
 		this.age = age;
 	}
 	
-	//Findest first occurrence of matching 'Age' attrubute
+	/** findClosest() 	Returns first occurrence of matching 'Age' attrubute
+	 * 
+	 * @param Objects 	ArrayList of all current AGES to compare
+	 * @return int		The index in ArrayList of the first matching attribute, -1 if none match
+	 */
 	public int findClosest(ArrayList<Age> Objects){
 		for(Age a : Objects){
 			int i = 0;
@@ -21,10 +35,18 @@ public enum Age {
 		return -1; //No matching attributes
 	}
 	
+	/** getAge()		Return the string value of age
+	 * 
+	 * @return String	Return age
+	 */
 	public String getAge() {
         return age;
     }
 	
+	/** toString()		Return the string value of age
+	 * 
+	 * 	@return			Return age
+	 */	 
 	public String toString(){
 		return this.age;
 	}
--- src/VideoGames.java
+++ src/VideoGames.java
@@ -4,6 +4,7 @@ public class VideoGames {
 	private Age age;
 	private NUMclass hoursOfGameplay;
 	private NUMclass yearReleased;
+	
 	public VideoGames(NUMclass price, Age age, NUMclass hoursOfGameplay, NUMclass yearReleased)
 	{
 		this.price = price;
@@ -11,27 +12,35 @@ public class VideoGames {
 		this.hoursOfGameplay = hoursOfGameplay;
 		this.yearReleased = yearReleased;
 	}
+	
 	public NUMclass getPrice() {
 		return price;
 	}
+	
 	public void setPrice(NUMclass price) {
 		this.price = price;
 	}
+	
 	public Age getAge() {
 		return age;
 	}
+	
 	public void setAge(Age age) {
 		this.age = age;
 	}
+	
 	public NUMclass getHoursOfGameplay() {
 		return hoursOfGameplay;
 	}
+	
 	public void setHoursOfGameplay(NUMclass hoursOfGameplay) {
 		this.hoursOfGameplay = hoursOfGameplay;
 	}
+	
 	public NUMclass getYearReleased() {
 		return yearReleased;
 	}
+	
 	public void setYearReleased(NUMclass yearReleased) {
 		this.yearReleased = yearReleased;
 	}
