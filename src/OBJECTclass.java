c91c9525931654948b5eb891ce182dc0030a9da4
Callum <Callum@CKirbs>
2017-11-04 13:01:59 -0400

    --

--- src/OBJECTclass.java
+++ src/OBJECTclass.java
@@ -1,4 +1,6 @@
 
 public class OBJECTclass {
-// General class to represent various data type storage requests 
+// General class to represent various data type storage requests
+	//Track number of attributes
+	//Track number of objects
 }
