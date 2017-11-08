8d0d57ad720e133bd130cf22dd008fe2c46410e2
BHartford <hartbrand99@gmail.com>
2017-11-08 12:08:04 -0500

    added empty View class

---
+++ src/KNNView.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.util.*;
+public class KNNView extends JFrame implements Observer{
+	public KNNView()
+	{
+		super("KNN");
+	}
+	public void update(Observable obs, java.lang.Object obj)
+	{
+		
+	}
+}
