9c5030e70463322dbde9e02699e45807d68f5a0d
christophermolnar <cmolnar22@gmail.com>
2017-11-08 16:21:58 -0500

    Updated Object and Project feat Geoff

--- src/Object.java
+++ src/Object.java
@@ -32,4 +32,37 @@ public class Object {
 		}
 		return closest;
 	}
+	
+	public double LoopThroughData(){
+		
+		double totalScore = 0;
+		
+		for (Type t: this.getData()){
+			if (t instanceof Point){
+				//Point t2 = (Point) t;
+				//for (int position = 0; position < t2.getNums().size(); position ++ ){
+				//	System.out.println("Point " + t2.getNums().get(position) + " at position " + position);
+				//}
+			}
+			
+			else if (t instanceof Key){
+				System.out.println(((Key) t).getWord());
+			}
+			
+			else if (t instanceof Num){
+				System.out.println("Num");
+			}
+			
+			else{
+				System.out.println("OH NO :(");
+			}
+		}
+		
+		return totalScore;
+	}
+	
+	public double getScore(){
+		return (this.LoopThroughData());
+	}
+	
 }
