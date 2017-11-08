3cfea05ccec44871ab0a66da0eab8aeae0323dfa
joe <joe@mainpc.hitronhub.home>
2017-11-08 15:59:54 -0500

    added layout of view

--- src/KNNView.java
+++ src/KNNView.java
@@ -1,12 +1,73 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.*;
 public class KNNView extends JFrame implements Observer{
+	JMenuItem create, add;
+	JPanel input, output;
 	public KNNView()
 	{
 		super("KNN");
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menuCreate = new JMenu("Example");
+		create = new JMenuItem("Create");
+		add = new JMenuItem("Add");
+		add.setEnabled(false);
+		menuCreate.add(create);
+		menuCreate.add(add);
+		menuBar.add(menuCreate);
+		setJMenuBar(menuBar);
+		setLayout (new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)); 
+		addNewExample();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		addNewExampleCase();
+		setSize(800,800);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	public void update(Observable obs, java.lang.Object obj)
 	{
 		
+	}
+	private void addNewExample()
+	{
+		input = new JPanel();
+		input.setPreferredSize(new Dimension(800,200));
+		input.setLayout(new BoxLayout(input, BoxLayout.Y_AXIS));
+		output = new JPanel();
+		output.setPreferredSize(new Dimension(800,200));
+		add(input);
+		add(output);
+		add.setEnabled(true);
+	}
+	private void addNewExampleCase()
+	{
+		String[] age = {"New", "Old"};
+		FlowLayout flow = new FlowLayout();
+		flow.setHgap(20);
+		JTextField f1 = new JTextField("Coordinates");
+		JTextField f2 = new JTextField("sq. ft");
+		JTextField f3 = new JTextField("Price");
+		f1.setPreferredSize(new Dimension(100, 20));
+		f2.setPreferredSize(new Dimension(100, 20));
+		f3.setPreferredSize(new Dimension(100, 20));
+		JComboBox combo = new JComboBox(age);
+		JPanel p = new JPanel();
+		p.setLayout(flow);
+		p.add(f1);
+		p.add(f2);
+		p.add(combo);
+		p.add(f3);
+		input.add(p);
 	}
 }
