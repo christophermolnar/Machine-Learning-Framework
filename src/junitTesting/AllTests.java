62dcdf8593389347853865d9db25bd3ca103863f
Geoff <geoff.software@outlook.com>
2017-11-23 12:48:52 -0500

    Created junit package, AllTests Class

---
+++ src/junitTesting/AllTests.java
@@ -0,0 +1,14 @@
+package junitTesting;
+import junit.framework.*;
+
+
+public class AllTests extends TestSuite {
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(AllTests.suite());
+	}
+	
+	public static Test suite(){
+		TestSuite suite = new TestSuite();
+		return suite;
+	}
+}
