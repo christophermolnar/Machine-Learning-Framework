d450be7109fedfe9a1aba4deca370b1b7f0e41c7
Callum <Callum@CKirbs>
2017-12-10 14:14:12 -0500

    Import B

--- src/machineLearning/KNNModel.java
+++ src/machineLearning/KNNModel.java
@@ -538,7 +538,8 @@ public class KNNModel extends Observable implements Serializable{
 			MachineLearning.importProject(importedProject);
 			streamIn.close();
 		 } catch (Exception e) {
-			 JOptionPane.showMessageDialog(null, "Please Check File Name and File exists within the Source Directory", "Import Error!", JOptionPane.PLAIN_MESSAGE);
+			 if(filename == null)
+				 JOptionPane.showMessageDialog(null, "Please Check File Name and File exists within the Source Directory", "Import Error!", JOptionPane.PLAIN_MESSAGE);
 		 }
 	}
 	public void out(String filename)
